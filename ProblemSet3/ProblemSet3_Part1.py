2678b392701b031e9d8d708b95bfc6742bee61d9
allyaplin <allison.aplin@duke.edu>
2024-10-02 12:16:12 -0400

    PS3 part 1 #1

---
+++ ProblemSet3/ProblemSet3_Part1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------
+# ProblemSet3_Part1.py
+#
+#Description: Problem Set 3 for Writing Scripts practice in VS Code
+# Author: Ally Aplin (allison.aplin@duke.edu)
+# Date:   Fall 2024
+#----------
+#%% Task 1 - Edit code to print as requested
+# Variables
+mountain = "Denali"
+nickname = '"Mt. McKinley"' #add quotes as well
+elevation = 20322  # Corrected the assignment operator == to =
+
+# Minimal edits to achieve correct output
+print(mountain + ", formerly\nknown as " + nickname)  # Use \n for new lines and change quotes for nickname
+print("is " + str(elevation) + "' feet above sea level.")  # Add str() to convert elevation to a string and add ' after
+
+#%% Task 2 - Lists and Iteration
